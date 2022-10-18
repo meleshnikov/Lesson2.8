4cc8a11ee50a0c94b57da21759e0027415aeb73e
Andrew Meleshnikov <andreymeleshnikov@gmail.com>
2022-10-18 23:43:30 +0300

    task 3.2 refactored

--- src/hw3/task2/Solution.java
+++ src/hw3/task2/Solution.java
@@ -9,28 +9,21 @@ public class Solution {
     public static void main(String[] args) {
 
         System.out.println("1 способ");
-        printRandomMultiplicationTable1(2, 9, 15);
+        printRandomMultiplicationTable1(2, 9, -1);
 
         System.out.println("\n2 способ");
-        printRandomMultiplicationTable2(2, 9, 15);
+        printRandomMultiplicationTable2(2, 9, -1);
     }
 
 
     //1 способ с применением List
     private static void printRandomMultiplicationTable1(int from, int to, int count) {
-
-        if (count < 0) {
-            throw new IllegalArgumentException("Количество примеров не может быть отрицательным");
-        }
-
         if (from > to) {
             int tmp = from;
             from = to;
             to = tmp;
         }
-
         ArrayList<String> allExamples = new ArrayList<>();
-
         // заполняем list всеми возможными примерами из диапазона from - to
         for (int i = from; i <= to; i++) {
             for (int j = i; j <= to; j++) {
@@ -41,7 +34,7 @@ public class Solution {
         // перемешиваем
         Collections.shuffle(allExamples);
 
-        if (count > allExamples.size()) {
+        if ((count > allExamples.size()) || (count < 0)) {
             count = allExamples.size();
         }
 
@@ -49,28 +42,23 @@ public class Solution {
         for (int i = 0; i < count; i++) {
             System.out.println(allExamples.get(i));
         }
+
+        System.out.println(count);
     }
 
     // 2 способ с применением Set
     private static void printRandomMultiplicationTable2(int from, int to, int count) {
-
-        if (count < 0) {
-            throw new IllegalArgumentException("Количество примеров не может быть отрицательным");
-        }
-
         if (from > to) {
             int tmp = from;
             from = to;
             to = tmp;
         }
         // количество всех цифр таблицы умножения
-        int n = Math.abs(from + 1 - to);
-
+        int n = Math.abs(to + 1 - from);
         // максимальное количество возможных вариантов примеров
         // сумма арифметической прогрессии от 1 до n
         int maxCount = (1 + n) * n / 2;
-
-        if (count > maxCount) {
+        if ((count > maxCount) || (count < 0)) {
             count = maxCount;
         }
 
@@ -87,6 +75,8 @@ public class Solution {
         for (String exam : examples) {
             System.out.println(exam);
         }
+
+        System.out.println(count);
     }
 
     private static int randomInt(int from, int to) {
